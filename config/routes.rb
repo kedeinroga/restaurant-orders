d96750bfc408fa955212920cf5baeea87b54aacc
kedeinroga <keybroga@gmail.com>
2020-12-29 19:56:29 -0500

    added and edited view root, creadted new controller Core because it was necessary for the root view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  root 'orders#index'
+  root 'core#index'
+  resources :core, only: :index
   resources :orders
-  resources :ingredients
+  resources :ingredients, except: :index
   resources :dishes
   resources :clients
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
